a19d26bccb6c8e3a4c98e77e503b547f5f4bb48c
niltor <zpty@outlook.com>
2024-04-21 12:29:02 +0800

    初始化数据

--- src/Http.API/Worker/InitDataWorker.cs
+++ src/Http.API/Worker/InitDataWorker.cs
@@ -8,21 +8,28 @@ public class InitDataWorker
     public static async Task InitAsync(IServiceProvider serviceProvider)
     {
         using var scope = serviceProvider.CreateScope();
-
         var context = scope.ServiceProvider.GetRequiredService<CommandDbContext>();
         await context.Database.MigrateAsync();
 
+        var logger = scope.ServiceProvider.GetRequiredService<ILogger<InitDataWorker>>();
+
         var systemUserManager = scope.ServiceProvider.GetRequiredService<SystemUserManager>();
 
-        await InitSystemUserAsync(systemUserManager);
+        await InitSystemUserAsync(systemUserManager, logger);
     }
 
-    //private static async Task InitOpenIdApplicationAsync(IOpenIddictApplicationManager manager)
-    //{
+    private static async Task InitConfigsAsync(CommandDbContext context, ILogger<InitDataWorker> logger)
+    {
+        if (!context.Configs.Any(c => c.Group.Equals(Constants.Config.DefaultGroup)))
+        {
+            var configs = Config.DefaultPermissionConfigs();
+            context.AddRange(configs);
+            await context.SaveChangesAsync();
+            logger.LogInformation("Init configs");
+        }
+    }
 
-    //}
-
-    private static async Task InitSystemUserAsync(SystemUserManager manager)
+    private static async Task InitSystemUserAsync(SystemUserManager manager, ILogger<InitDataWorker> logger)
     {
         var exist = await manager.FindAsync<SystemUser>(u => u.UserName == "admin");
         if (exist == null)
@@ -32,9 +39,9 @@ public class InitDataWorker
                 UserName = "admin",
                 Email = "admin@ater.com",
                 Password = "docs.dusi.dev"
-
             };
             await manager.CreateNewEntityAsync(user);
+            logger.LogInformation("Init systemUse:{UserName}/{Password}", user.UserName, user.Password);
         }
     }
 }
